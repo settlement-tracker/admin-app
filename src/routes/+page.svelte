e58f164e9a362005b1ddb9614ee54c3e9b2322c8
trevo <trevorius45@gmail.com>
2023-06-08 21:38:07 -0400

    install sass

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button class="btn btn-secondary text-primary">Test</button>
+
+<style lang="scss">
+	button {
+		background-color: $red;
+	}
+</style>
