cc2f97ef16b450ead4ed05f0e8f8ed4af1ca72dd
trevo <trevorius45@gmail.com>
2023-06-09 21:05:42 -0400

    test that css is loaded

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,20 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<button class="btn btn-secondary text-primary">Test</button>
+<div class="test-variable-auto-loading">
+	<p>variables are loading if the background for this is red</p>
+</div>
+
+<button class="btn btn-primary">Test bootsrap vraiable overides</button>
+
+<div class="navbar-vertical pheonix-theme-test">
+	<button class="btn btn-phoenix-secondary">test phoenix specific classes</button>
+</div>
+
+<div class="dark" />
 
 <style lang="scss">
-	button {
+	.test-variable-auto-loading {
 		background-color: $red;
 	}
 </style>
